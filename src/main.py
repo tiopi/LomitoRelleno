83f3546e1dc3d7955964aa0281f949f02c4fb89d
Osmar Coronel <osmar@Osmars-MacBook-Pro.local>
2020-04-30 00:09:33 -0400

    Added stuff

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from utils import get_contract_from_blockchain
 from compiler import compile_into_ast
+from analyze import analyze_ast
 import json
 
 if __name__ == "__main__":
@@ -7,8 +8,4 @@ if __name__ == "__main__":
 	# src = get_contract_from_blockchain("0x5F098176B4f96207b3dc7b257175208112147243")
 	src_path, _ = get_contract_from_blockchain("0xC39D185eE1256E10D5010722D359ec87301eb647")
 	ast = compile_into_ast(src_path)
-	print(ast.keys())
-	print(ast['nodes'])
-	# print(len(src))
-	# print(src[0].keys())
-	# print(src[0]['SourceCode'])
+	analyze_ast(ast)
